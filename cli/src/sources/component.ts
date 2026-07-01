76ce3129687bd9dcbb7bd871273d1047bc11f459
Eric Hartye <eric@hartye.com>
2026-06-30 20:57:58 -0400

    feat: component-library source (cx-router)

---
+++ cli/src/sources/component.ts
@@ -0,0 +1,32 @@
+import type { BrowserManager } from "../browser";
+import type { ComponentRef, DocResult } from "../types";
+
+export function componentUrl(ref: ComponentRef): string {
+  return `https://developer.salesforce.com/cx-router/components?model=${ref.model}&namespace=${ref.namespace}&component=${ref.name}`;
+}
+
+export async function fetchComponent(browser: BrowserManager, ref: ComponentRef): Promise<DocResult> {
+  const url = componentUrl(ref);
+  const json = await browser.fetchJsonInPage(url);
+  const r = json.response ?? {};
+  const title = `${ref.namespace}-${ref.name}`;
+  const lines = [
+    `# ${title}`,
+    "",
+    `> Source: ${url}`,
+    `> Retrieved via sf-docs (component)`,
+    "",
+    r.global?.description ?? "",
+    "",
+    "## Attributes",
+    "",
+    "| Attribute | Required | Description |",
+    "| --- | --- | --- |",
+    ...(r.attributes ?? []).map(
+      (a: any) => `| ${a.nameInKebabCase ?? a.name} | ${a.required ? "yes" : "no"} | ${(a.description ?? "").replace(/\|/g, "\\|")} |`,
+    ),
+    "",
+  ];
+  const markdown = lines.join("\n");
+  return { title, url, source: "component", version: r.global?.support, html: "", markdown };
+}
